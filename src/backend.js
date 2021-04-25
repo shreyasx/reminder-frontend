a0a95b79ce7b709c7dd26deb661ae8e899224f6a
Shreyas <shreyxs@gmail.com>
2021-04-25 22:25:18 +0530

    added link to new API

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,4 @@
 // export const API = "http://localhost:5000";
-export const API = "https://morning-wildwood-39169.herokuapp.com";
+// export const API = "https://morning-wildwood-39169.herokuapp.com";
+export const API = "https://reminder-api-bt75d.ondigitalocean.app";
 // export const API = "http://192.168.0.186:5000";
