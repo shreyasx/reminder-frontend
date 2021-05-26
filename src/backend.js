852d6058ce0f08fea1bb77964045e014911b099c
Shreyas <shreyxs@gmail.com>
2021-05-26 16:37:16 +0530

    bug fixes

--- src/backend.js
+++ src/backend.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:5000";
+// export const API = "http://localhost:5000";
 // export const API = "https://morning-wildwood-39169.herokuapp.com";
-// export const API = "https://reminder-api-kvo37.ondigitalocean.app";
+export const API = "https://reminder-api-kvo37.ondigitalocean.app";
 // export const API = "http://192.168.0.186:5000";
