72021dd02098b892ae988f80f0d04cc5e03bea34
Shreyas <shreyxs@gmail.com>
2021-08-06 22:35:14 +0530

    changed API url

--- src/backend.js
+++ src/backend.js
@@ -1,2 +1,2 @@
-export const API = "http://192.168.0.186:5000";
-// export const API = "https://shreyas-reminder-api.herokuapp.com";
+// export const API = "http://192.168.0.186:5000";
+export const API = "https://shreyas-reminder-api.herokuapp.com";
