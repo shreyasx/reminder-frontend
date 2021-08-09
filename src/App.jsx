c6d7bedb035bdce64ba62f1c06a67a5fad5d80a5
Shreyas <shreyxs@gmail.com>
2021-08-09 08:42:10 +0530

    added update option

--- src/App.jsx
+++ src/App.jsx
@@ -22,9 +22,11 @@ const App = () => {
 		}
 	};
 
-	console.log(`serviceWorkerUpdated ${sw.serviceWorkerUpdated}`);
-	console.log(`serviceWorkerRegistration`, sw.serviceWorkerRegistration);
-	console.log(`serviceWorkerInitialized ${sw.serviceWorkerInitialized}`);
+	React.useEffect(() => {
+		console.log(`serviceWorkerUpdated ${sw.serviceWorkerUpdated}`);
+		console.log(`serviceWorkerRegistration`, sw.serviceWorkerRegistration);
+		console.log(`serviceWorkerInitialized ${sw.serviceWorkerInitialized}`);
+	}, [sw]);
 
 	return (
 		<BrowserRouter>
