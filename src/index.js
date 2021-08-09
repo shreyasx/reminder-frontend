a5136d433253498129f2cc2f4a8a5eb9647d2c71
Shreyas <shreyxs@gmail.com>
2021-08-09 08:54:42 +0530

    added update option

--- src/index.js
+++ src/index.js
@@ -30,5 +30,16 @@ ReactDOM.render(
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register({
 	onSuccess: () => store.dispatch({ type: "SW_INIT" }),
-	onUpdate: reg => store.dispatch({ type: "SW_UPDATE", payload: reg }),
+	onUpdate: registration => {
+		const waitingServiceWorker = registration.waiting;
+
+		if (waitingServiceWorker) {
+			waitingServiceWorker.addEventListener("statechange", event => {
+				if (event.target.state === "activated") {
+					window.location.reload();
+				}
+			});
+			waitingServiceWorker.postMessage({ type: "SKIP_WAITING" });
+		}
+	},
 });
